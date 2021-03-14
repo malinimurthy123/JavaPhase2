b78c7c5cfde0ebd9b02d01a4160887cdc2afefa1
“malinimurthy123git config --global user.name “malinimurthy123 <krmalini77@gmail.comgit config --global user.email krmalini77@gmail.com>
2021-03-14 10:05:42 -0700

    Session Handling

---
+++ src/main/java/com/example/SessinHandlingExample/Authenticate.java
@@ -0,0 +1,46 @@
+package com.example.SessinHandlingExample;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+
+@WebServlet("/authenticate")
+public class Authenticate extends HttpServlet {
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+//		Set the content type
+        response.setContentType("text/html");
+
+//		Get the writer
+        PrintWriter out = response.getWriter();
+
+        //		Fetch the values from Request
+        String userName = request.getParameter("userName");
+//		String userPassword = request.getParameter("userPass");
+
+//		Get a reference to the Session
+        HttpSession theSession = request.getSession();
+
+//		Check whether parameters exists in the request
+        if(userName !=  null) {
+//			Save the userName(coming from the request) into the session
+            theSession.setAttribute("uName", userName);
+
+        }
+
+//		Print userName from request
+        out.println("Welcome " + userName + "<br>");
+
+//		Print userName from session
+        out.println("Welcome " + theSession.getAttribute("uName"));
+
+    }
+
+}
---
+++ target/SessinHandlingExample-1.0-SNAPSHOT/index.jsp
@@ -0,0 +1,17 @@
+<%@ page contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" %>
+<!DOCTYPE html>
+
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Home Page</title>
+</head>
+<body>
+<form action="authenticate">
+    <input type="text" placeholder="Your Name" name="userName"><br>
+    <!-- <input type="password" placeholder="Your Password" name="userPass"><br> -->
+    <input type="submit" value="LOGIN">
+
+</form>
+</body>
+</html>

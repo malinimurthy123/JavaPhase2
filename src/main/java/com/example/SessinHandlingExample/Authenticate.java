37f256ac21a45b561b3d8691d9fb930fc9a7508a
“malinimurthy123git config --global user.name “malinimurthy123 <krmalini77@gmail.comgit config --global user.email krmalini77@gmail.com>
2021-03-19 23:46:34 -0700

    Session Handling

--- src/main/java/com/example/SessinHandlingExample/Authenticate.java
+++ src/main/java/com/example/SessinHandlingExample/Authenticate.java
@@ -1,5 +1,6 @@
 package com.example.SessinHandlingExample;
 
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -21,6 +22,12 @@ public class Authenticate extends HttpServlet {
 //		Get the writer
         PrintWriter out = response.getWriter();
 
+//		Fetch the value from the hidden form field, which was sent along this request
+        String id =request.getParameter("userId");
+
+        System.out.println("ID : " + id);
+
+
         //		Fetch the values from Request
         String userName = request.getParameter("userName");
 //		String userPassword = request.getParameter("userPass");
@@ -28,19 +35,27 @@ public class Authenticate extends HttpServlet {
 //		Get a reference to the Session
         HttpSession theSession = request.getSession();
 
+//		Get a reference to the ServletContext object
+        ServletContext theContext = getServletContext();
+
 //		Check whether parameters exists in the request
         if(userName !=  null) {
 //			Save the userName(coming from the request) into the session
             theSession.setAttribute("uName", userName);
 
+//			Save the userName(coming from the request) into the context
+            theContext.setAttribute("uName", userName);
+
         }
 
 //		Print userName from request
-        out.println("Welcome " + userName + "<br>");
+        out.println("<h3>From Request, username : " + userName + "<br>");
 
 //		Print userName from session
-        out.println("Welcome " + theSession.getAttribute("uName"));
+        out.println("From session, username : " + theSession.getAttribute("uName") + "<br>");
 
+//		Print userName from context
+        out.println("From context, username : " + theContext.getAttribute("uName") + "</h3>");
     }
 
 }
